e49741b5c405d8ddd7af29f0b5b5687c963591d2
kz4 <zkcneu@gmail.com>
2016-12-04 22:37:01 -0500

    map.py does not go into the run()

--- ContentDeliveryNetwork/map.py
+++ ContentDeliveryNetwork/map.py
@@ -6,63 +6,87 @@ lock = Lock()
 import sys
 
 # Original server
-replicaIps = ['52.4.98.110']
+replicaIps = ['54.210.1.206']
 # Key: client IP, value: (ping time, replica IP, last request time)
-clientIpMap = {}
+clientIpMap = {'192.168.205.201' : (30000, '', time.time())}
 
 
 class Map(object):
-	def __init__(self, port):
-		self.getPingTime()
-		self.port = port
-		self.threads = []
-		self.initThreads()
+    def __init__(self, port):
+        self.port = port
+        self.threads = []
+        self.initThreads()
+        self.startThread()
 
-	def initThreads(self):
-		for ip in replicaIps:
-			thread = replicaThread(ip, self.port)
-			thread.daemon = True
-			self.threads.append(thread)
-			thread.start()
+    def initThreads(self):
+        # for ip in replicaIps:
+        #     print 'initThreads: ip: ', ip
+        #     thread = replicaThread(ip, self.port)
+        #     thread.daemon = True
+        #     self.threads.append(thread)
+        #     thread.start()
+
+        # ip = replicaIps[0]
+        # print 'initThreads: ip: ', ip
+        # thread = replicaThread(ip, self.port)
+        # thread.daemon = True
+
+        for ip in replicaIps:
+            print 'initThreads: ip: ', ip
+            thread = replicaThread(ip, self.port)
+            thread.daemon = True
+            self.threads.append(thread)
+
+    def startThread(self):
+        for thread in self.threads:
+            thread.start()
 
 class replicaThread(Thread):
-	def __init__(self, ip, port):
-		Thread.__init__(self)
-		self.ip = ip
-		# Socket used to communicate client IP with Ping Server (pingServer.py)
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.port = port
-		self.sock.settimeout(1)
+    def __init__(self, ip, port):
+        Thread.__init__(self)
+        self.ip = ip
+        # Socket used to communicate client IP with Ping Server (pingServer.py)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.port = port
+        self.sock.settimeout(1)
+        print 'replicaThread init'
 
-	def run(self):
-		while True:
-			lock.acquire()
-			for clientIp in clientIpMap:
-				# 600 seconds
-				if time.time() - clientIpMap[clientIp][2] > 600:
-					clientIpMap.pop(clientIp)
-			lock.release()
-			try:
-				clientIpTimeSpanJson = json.dumps({'PingRequest': \
-										{'ClientIps' : clientIpMap.keys(), \
-										'TimeSpan' : time.time(), 'ReplicaIp' : self.ip}})
-				self.sock.sendto(clientIpTimeSpanJson, (self.ip, self.port))
+    def run(self):
+        print 'entering run'
+        while True:
+            print 'run'
+            lock.acquire()
+            for clientIp in clientIpMap:
+                # 600 seconds
+                if time.time() - clientIpMap[clientIp][2] > 600:
+                    clientIpMap.pop(clientIp)
+            lock.release()
+            try:
+                clientIpTimeSpanJson = json.dumps({'PingRequest': \
+                                        {'ClientIps' : clientIpMap.keys(), \
+                                        'TimeSpan' : time.time(), 'ReplicaIp' : self.ip}})
+                print 'clientIpTimeSpanJson: ', clientIpTimeSpanJson
+                self.sock.sendto(clientIpTimeSpanJson, (self.ip, self.port))
 
-				resJson, replicaIp = self.sock.recvfrom(1024)
-				if replicaIp == self.ip:
-					res = json.loads(resJson)
-					if res.has_key('PingResult'):
-						pingTimeDict = res['PingResult']['PingTimeDict']
-						lock.acquire()
-						for clientIp in pingTimeDict:
-							# If client IPs are not active for some time, we remove them from
-							if clientIp in clientIpMap and clientIpMap[clientIp][0] > pingTimeDict[clientIp]:
-								lastTime = clientIpMap[clientIp][2]
-								clientIpMap[clientIp] = (pingTimeDict[clientIp], self.ip, lastTime)
-						lock.release()
-			except:
-				pass
-			time.sleep(1)
+                print 'Map send'
+                resJson, replicaIp = self.sock.recvfrom(1024)
+                print 'resJson, replicaIp: ', resJson, replicaIp
+                if replicaIp == self.ip:
+                    res = json.loads(resJson)
+                    if res.has_key('PingResult'):
+                        pingTimeDict = res['PingResult']['PingTimeDict']
+                        lock.acquire()
+                        for clientIp in pingTimeDict:
+                            # If client IPs are not active for some time, we remove them from
+                            if clientIp in clientIpMap and clientIpMap[clientIp][0] > pingTimeDict[clientIp]:
+                                lastTime = clientIpMap[clientIp][2]
+                                clientIpMap[clientIp] = (pingTimeDict[clientIp], self.ip, lastTime)
+                                print 'updated clientIpMap: ', clientIpMap
+                        lock.release()
+            except:
+                pass
+            time.sleep(1)
 
 if __name__ == '__main__':
-	map = Map(sys.argv[1])
+    map = Map(55555)
+    # map = Map(sys.argv[1])
--- ContentDeliveryNetwork/pingServer.py
+++ ContentDeliveryNetwork/pingServer.py
@@ -2,41 +2,54 @@ import socket
 import json
 
 class PingServer(object):
-	def __init__(self, port):
-		# Socket used to communicate RTT with Map (map.py)
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.port = port
-		self.sock.settimeout(1)
-		# TODO: Change 52.4.98.110 (EC2 IP) to server's own IP
-		self.ip = '52.4.98.110'
-		self.sock.bind((self.ip, port))
+    def __init__(self, port):
+        # Socket used to communicate RTT with Map (map.py)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.port = port
+        # self.sock.settimeout(1)
+        # TODO: Change 52.4.98.110 (EC2 IP) to server's own IP
+        # self.ip = '54.210.1.206'
+        # self.ip = '129.10.106.91'
+        # self.ip = '54.167.4.20' #original EC2
+        # self.ip = '129.10.117.186' # cs5700cdnproject.ccs.neu.edu
+        self.ip = '' # cs5700cdnproject.ccs.neu.edu
 
-	def run(self):
-		while True:
-			# clientIpTimeSpanJson = json.dumps({'PingRequest': \
-			# 							{'ClientIps' : clientIpMap.keys(), \
-			# 							'TimeSpan' : time.time(), 'ReplicaIp' : self.ip}})
-			reqJson, replicaIp = self.sock.recvfrom(1024)
-			res = json.loads(resJson)
-			if res.has_key('PingRequest') and res['PingRequest']['ReplicaIp'] == self.ip:
-				clientIps = res['PingRequest']['ClientIps']
-				resultDict = {}
-				for clientIp in clientIps:
-					rtt = getPingTime(clientIp)
-					if rtt:
-						resultDict[clientIp] = rtt
-				self.sendPingResult(resultDict)
+        self.sock.bind((self.ip, port))
 
-	def sendPingResult(self, resultDict):
-		resultJson = json.dumps({'PingResult' : {'PingTimeDict' : resultDict}})
-		self.sock.sendto(resultJson, (self.ip, self.port))
+    def run(self):
+        while True:
+            # clientIpTimeSpanJson = json.dumps({'PingRequest': \
+            #                           {'ClientIps' : clientIpMap.keys(), \
+            #                           'TimeSpan' : time.time(), 'ReplicaIp' : self.ip}})
+            reqJson, replicaIp = self.sock.recvfrom(1024)
+            print 'reqJson, replicaIp: ', reqJson, replicaIp
+            res = json.loads(resJson)
+            if res.has_key('PingRequest') and res['PingRequest']['ReplicaIp'] == self.ip:
+                clientIps = res['PingRequest']['ClientIps']
+                resultDict = {}
+                for clientIp in clientIps:
+                    rtt = getPingTime(clientIp)
+                    print 'clientIp: ', clientIp
+                    print 'rtt: ', rtt
+                    if rtt:
+                        resultDict[clientIp] = rtt
+                self.sendPingResult(resultDict)
 
-	def getPingTime(self, clientIp):
-		command = "scamper -c 'ping -c 1' -i " + clientIp + " | grep 'time='"
+    def sendPingResult(self, resultDict):
+        resultJson = json.dumps({'PingResult' : {'PingTimeDict' : resultDict}})
+        self.sock.sendto(resultJson, (self.ip, self.port))
+        print 'finish sending in PingServer'
+
+    def getPingTime(self, clientIp):
+        command = "scamper -c 'ping -c 1' -i " + clientIp + " | grep 'time='"
         outputs = os.popen(command).read()
         print outputs
         if outputs.find('time='):
             m = re.search('time=([0-9]*.[0-9]*)', outputs)
             if m:
-        		return float(m.group(1))
-		return None
+                return float(m.group(1))
+        return None
+
+if __name__ == '__main__':
+    pingServer = PingServer(55555)
+    pingServer.run()
